a0d69bdf79bc3b73506a921894cd85c87c9b69d3
Eratorn <50834416+Eratorn@users.noreply.github.com>
2019-06-05 16:29:57 -0500

    Create JuegoDeLaVida.java

---
+++ JuegoDeLaVida.java
@@ -0,0 +1,15 @@
+package juegodelavida;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+public class JuegoDeLaVida extends JFrame{
+    public JuegoDeLaVida(int w, int h){
+        super("Juego de la vida");
+        this.setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-w)/2, (Toolkit.getDefaultToolkit().getScreenSize().height-h)/2, w, h);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.add(new Panel());
+        
+    }
+}
