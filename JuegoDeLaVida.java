26a20ac7fde25a03bcf39d34e61783bbc0a8c928
Eratorn <50834416+Eratorn@users.noreply.github.com>
2019-06-05 16:57:53 -0500

    Update JuegoDeLaVida.java

--- JuegoDeLaVida.java
+++ JuegoDeLaVida.java
@@ -7,6 +7,7 @@ public class JuegoDeLaVida extends JFrame{
         super("Juego de la vida");
         this.setBounds((Toolkit.getDefaultToolkit().getScreenSize().width-w)/2, (Toolkit.getDefaultToolkit().getScreenSize().height-h)/2, w, h);
         this.setVisible(true);
+        this.setFocusable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.add(new Panel());
